f8085a616c4a8cc0f664f42551c77e9ab3978ed2
Parkyeongmi98 <yeongmi_@naver.com>
2023-03-13 10:09:35 +0900

    event

---
+++ 2.component/ex03/src/02/TitleBar02.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function TitelBar02() {
+
+
+    return (
+        <div>
+            <h1>
+               Function Handler 
+            </h1>
+        </div>
+    )
+}
--- emaillist/frontend/src/Email.js
+++ emaillist/frontend/src/Email.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Email = ({no, firstName, lastName, email}) => {
     return (
-        <li >
+        <li>
             {firstName+lastName}
             <br/>
             {email}
+            <a href=''></a>
         </li>
     );
 };
 
 export default Email;
-
 
